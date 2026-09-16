ce6857ca8fe5e2790b4f1705916b06914bcf31ec
𝑹𝒂𝒇𝒇 𝑨𝒍𝒅𝒔 <117867334+xiv3r@users.noreply.github.com>
2026-09-16 21:31:38 +0800

    final

--- esp8266-dht22-sketch.ino
+++ esp8266-dht22-sketch.ino
@@ -3,26 +3,35 @@
 #define DHTPIN D2
 #define DHTTYPE DHT22
 #define RELAY_PIN D5
-#define TEMP_ON 35.0 // ON = 35 Degree Celsius
-#define TEMP_OFF 34.0 // OFF = 34 Degree Celsius 
+#define TEMP_ON 35.0
+#define TEMP_OFF 34.0
+#define READ_INTERVAL 2000
 
 DHT dht(DHTPIN, DHTTYPE);
 
 bool relayState = false;
+unsigned long lastRead = 0;
 
 void setup() {
+  pinMode(DHTPIN, INPUT_PULLUP);
   dht.begin();
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, LOW);
 }
 
 void loop() {
+  unsigned long now = millis();
+
+  if (now - lastRead < READ_INTERVAL) {
+    return;
+  }
+  lastRead = now;
+
   float temperature = dht.readTemperature();
 
   if (isnan(temperature)) {
     digitalWrite(RELAY_PIN, LOW);
     relayState = false;
-    delay(2000);
     return;
   }
 
@@ -33,6 +42,4 @@ void loop() {
     relayState = false;
     digitalWrite(RELAY_PIN, LOW);
   }
-
-  delay(2000);
 }
