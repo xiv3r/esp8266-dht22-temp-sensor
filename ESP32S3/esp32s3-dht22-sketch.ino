13c275985aece159101eeb8835ad4eb48a68038d
𝑹𝒂𝒇𝒇 𝑨𝒍𝒅𝒔 <117867334+xiv3r@users.noreply.github.com>
2026-09-17 05:59:00 +0800

    final

---
+++ ESP32S3/esp32s3-dht22-sketch.ino
@@ -0,0 +1,45 @@
+#include <DHT.h>
+
+#define DHTPIN 4
+#define DHTTYPE DHT22
+#define RELAY_PIN 5
+#define TEMP_ON 35.0
+#define TEMP_OFF 34.0
+#define READ_INTERVAL 2000
+
+DHT dht(DHTPIN, DHTTYPE);
+
+bool relayState = false;
+unsigned long lastRead = 0;
+
+void setup() {
+  pinMode(DHTPIN, INPUT_PULLUP);
+  dht.begin();
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW);
+}
+
+void loop() {
+  unsigned long now = millis();
+
+  if (now - lastRead < READ_INTERVAL) {
+    return;
+  }
+  lastRead = now;
+
+  float temperature = dht.readTemperature();
+
+  if (isnan(temperature)) {
+    digitalWrite(RELAY_PIN, LOW);
+    relayState = false;
+    return;
+  }
+
+  if (!relayState && temperature > TEMP_ON) {
+    relayState = true;
+    digitalWrite(RELAY_PIN, HIGH);
+  } else if (relayState && temperature < TEMP_OFF) {
+    relayState = false;
+    digitalWrite(RELAY_PIN, LOW);
+  }
+}
